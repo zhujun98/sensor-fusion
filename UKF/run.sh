74c33983c50bee574df5557e5f52c96b5233f026
Jun Zhu <zhujun981661@gmail.com>
2017-04-15 23:09:45 +0200

    Updated run.sh for UKF

--- UKF/run.sh
+++ UKF/run.sh
@@ -5,4 +5,9 @@ index=$1
 
 if [ ! $index ]; then index=1; fi
 
-cmake-build-debug/UnscentedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+if [ -f cmake-build-debug/UnscentedKF ] ; then
+    cmake-build-debug/UnscentedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+else
+    echo -e "\nSwitched to ./build directory"
+    build/Unscented data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+fi
