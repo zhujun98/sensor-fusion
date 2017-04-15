d6991bd8cb62452a0bb75f30b607b4af025dec27
Jun Zhu <zhujun981661@gmail.com>
2017-04-15 23:02:26 +0200

    Commited the compiled file in ./build/ and updated run.sh

--- EKF/run.sh
+++ EKF/run.sh
@@ -5,4 +5,9 @@ index=$1
 
 if [ ! $index ]; then index=1; fi
 
-cmake-build-debug/ExtendedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+if [ -f cmake-build-debug/ExtendedKF ] ; then
+    cmake-build-debug/ExtendedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+else
+    echo -e "\nSwitched to ./build directory"
+    build/ExtendedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+fi
