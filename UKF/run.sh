98ebf3c14c520e478dfcd28f826e5b9b10f62d42
Jun Zhu <zhujun981661@gmail.com>
2017-04-19 01:20:32 +0200

    Added a new data for UKF project.

--- UKF/run.sh
+++ UKF/run.sh
@@ -3,11 +3,17 @@
 
 index=$1
 
-if [ ! $index ]; then index=1; fi
+if [ ! $index ]; then
+    data=data/obj_pose-laser-radar-synthetic-input.txt
+    output=output/basic-output.txt
+else
+    data=data/sample-laser-radar-measurement-data-${index}.txt
+    output=output/basic-output-${index}.txt
+fi
 
 if [ -f cmake-build-debug/UnscentedKF ] ; then
-    cmake-build-debug/UnscentedKF data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+    cmake-build-debug/UnscentedKF $data $output
 else
     echo -e "\nSwitched to ./build directory"
-    build/Unscented data/sample-laser-radar-measurement-data-${index}.txt output/basic-output-${index}.txt
+    build/Unscented $data $output
 fi
