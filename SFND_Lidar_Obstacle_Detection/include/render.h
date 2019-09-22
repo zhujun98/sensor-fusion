b95fc4d3a09bcc326ff3f0cc9c1b00b0959f2e19
zhujun98 <zhujun981661@gmail.com>
2019-09-22 23:15:46 +0200

    Restructure the code and implement PcdStreamer.
    The design of the course project confuses me. It first
    creates several blocks (cars) and check the collision of lidar
    cloud, for the first data set. I removed those code and plan to
    localize the obstacles using the LIDAR data.

--- SFND_Lidar_Obstacle_Detection/include/render.h
+++ SFND_Lidar_Obstacle_Detection/include/render.h
@@ -18,108 +18,26 @@ struct Color
   {}
 };
 
-struct Vect3
-{
-  double x, y, z;
-
-  Vect3(double setX, double setY, double setZ)
-    : x(setX), y(setY), z(setZ)
-  {}
-
-  Vect3 operator+(const Vect3& vec)
-  {
-    Vect3 result(x+vec.x,y+vec.y,z+vec.z);
-    return result;
-  }
-};
-
 
 enum CameraAngle
 {
   XY, TopDown, Side, FPS
 };
 
-
-struct Car
-{
-  // units in meters
-  Vect3 position, dimensions;
-
-  std::string name;
-  Color color;
-
-  Car(Vect3 setPosition, Vect3 setDimensions, Color setColor, std::string setName)
-    : position(setPosition), dimensions(setDimensions), color(setColor), name(setName)
-  {}
-
-  void render(pcl::visualization::PCLVisualizer::Ptr& viewer)
-  {
-    // render bottom of a car
-    viewer->addCube(position.x - 0.5 * dimensions.x, position.x + 0.5 * dimensions.x,
-                    position.y - 0.5 * dimensions.y, position.y + 0.5 * dimensions.y,
-                    position.z, position.z + dimensions.z * 2/3,
-                    color.r, color.g, color.b, name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION,
-                                        pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE,
-                                        name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,
-                                        color.r, color.g, color.b, name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, name);
-
-    // render top of a car
-    auto top_name = name + "Top";
-
-    viewer->addCube(position.x - 0.25 * dimensions.x, 0.25 * position.x + dimensions.x,
-                    position.y - 0.5 * dimensions.y, position.y + 0.5 * dimensions.y,
-                    position.z + dimensions.z * 2/3, position.z + dimensions.z,
-                    color.r, color.g, color.b, top_name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION,
-                                        pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE, top_name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,
-                                        color.r, color.g, color.b, top_name);
-    viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, top_name);
-  }
-
-  // collision helper function
-  bool inbetween(double point, double center, double range)
-  {
-    return (center-range <= point) && (center+range >= point);
-  }
-
-  bool checkCollision(Vect3 point)
-  {
-    return (inbetween(point.x, position.x, dimensions.x/2) &&
-            inbetween(point.y, position.y, dimensions.y/2) &&
-            inbetween(point.z, position.z + dimensions.z / 3, dimensions.z / 3)) ||
-           (inbetween(point.x, position.x, dimensions.x/4) &&
-            inbetween(point.y, position.y, dimensions.y/2) &&
-            inbetween(point.z, position.z + dimensions.z * 5/6, dimensions.z / 6));
-
-  }
-};
-
-void renderHighway(pcl::visualization::PCLVisualizer::Ptr& viewer);
-
-void renderRays(pcl::visualization::PCLVisualizer::Ptr& viewer,
-                const Vect3& origin,
-                const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud);
-
-void clearRays(pcl::visualization::PCLVisualizer::Ptr& viewer);
-
 void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer,
                       const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
                       std::string name,
-                      Color color = Color(1,1,1));
+                      Color color = Color(1, 1, 1));
 
 void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer,
                       const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud,
                       std::string name,
-                      Color color = Color(-1,-1,-1));
+                      Color color = Color(-1, -1, -1));
 
 void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer,
-               Box box, int id, Color color = Color(1,0,0), float opacity=1);
+               Box box, int id, Color color = Color(1, 0, 0), float opacity=1);
 
 void renderBox(pcl::visualization::PCLVisualizer::Ptr& viewer,
-               BoxQ box, int id, Color color = Color(1,0,0), float opacity=1);
+               BoxQ box, int id, Color color = Color(1, 0, 0), float opacity=1);
 
 #endif // RENDER_H
--- SFND_Lidar_Obstacle_Detection/src/render.cpp
+++ SFND_Lidar_Obstacle_Detection/src/render.cpp
@@ -1,47 +1,6 @@
 #include "render.h"
 
 
-void renderHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
-{
-  // units in meters
-  double roadLength = 50.0;
-  double roadWidth = 12.0;
-  double roadHeight = 0.2;
-
-  viewer->addCube(-roadLength/2, roadLength/2, -roadWidth/2, roadWidth/2, -roadHeight, 0, .2, .2, .2, "highwayPavement");
-  viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION,
-                                      pcl::visualization::PCL_VISUALIZER_REPRESENTATION_SURFACE,
-                                      "highwayPavement");
-  viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, .2, .2, .2, "highwayPavement");
-  viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY, 1.0, "highwayPavement");
-  viewer->addLine(pcl::PointXYZ(-roadLength/2, -roadWidth/6, 0.01),
-                  pcl::PointXYZ(roadLength/2, -roadWidth/6, 0.01), 1, 1, 0, "line1");
-  viewer->addLine(pcl::PointXYZ(-roadLength/2, roadWidth/6, 0.01),
-                  pcl::PointXYZ(roadLength/2, roadWidth/6, 0.01), 1, 1, 0, "line2");
-}
-
-int countRays = 0;
-
-void renderRays(pcl::visualization::PCLVisualizer::Ptr& viewer,
-                const Vect3& origin,
-                const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
-{
-  for(pcl::PointXYZ point : cloud->points)
-  {
-    viewer->addLine(pcl::PointXYZ(origin.x, origin.y, origin.z), point, 1, 0, 0, "ray" + std::to_string(countRays));
-    ++countRays;
-  }
-}
-
-void clearRays(pcl::visualization::PCLVisualizer::Ptr& viewer)
-{
-  while(countRays)
-  {
-    --countRays;
-    viewer->removeShape("ray"+std::to_string(countRays));
-  }
-}
-
 void renderPointCloud(pcl::visualization::PCLVisualizer::Ptr& viewer,
                       const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud,
                       std::string name,
